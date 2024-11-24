f8252baaecab089d3c2e2f96262ff637a80c9575
augustos186 <acesar86@gmail.com>
2024-11-23 21:06:29 -0300

    second commit

--- teste/index.js
+++ teste/index.js
@@ -1,18 +1,18 @@
-let nome = "Superman"
-let xp = 1001
+let nome = "Batman"
+let xp = 5820
 if(xp < 1000)
-    console.log(nome, "Ferro")
+    console.log("O herói " + nome +  " é Ferro")
     else if(xp > 1000 && xp <= 2000)
-        console.log(nome, "Bronze")
+        console.log("O herói " + nome +  " é Bronze")
         else if(xp > 2000 && xp <= 5000)
-            console.log(nome, "Prata")
+            console.log("O herói " + nome +  " é Prata")
         else if(xp > 5000 && xp <= 7000)
-            console.log(nome, "Ouro")
+            console.log("O herói " + nome +  " é Ouro")
         else if(xp > 7000 && xp <= 8000)
-            console.log(nome, "Platina")
+            console.log("O herói " + nome +  " é Platina")
         else if(xp > 8000 && xp <= 9000)
-            console.log(nome, "Ascendente")
-        else if(xp > 9000 && xp <= 10000)
-            console.log(nome, "Imortal")
-        else
-            console.log(nome, "Radiante")
+            console.log("O herói " + nome +  " é Ascendente")
+        else if(xp > 9000 && xp <= 10000){
+            console.log("O herói " + nome +  " é Imortal");
+            }else
+            console.log("O herói " + nome +  " é Radiante")
